8b628cd05b6cd470caf76b4bc94b3b82a2ed702a
ajaykumar6666 <kumar.ajay44499@gmail.com>
2023-07-11 19:14:17 -0400

    leetcode medium

---
+++ customers_who_bought_all_products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select customer_id from(
+select customer_id,count(distinct product_key) from Customer
+group by customer_id
+having count(distinct product_key)=(select count(distinct product_key) from Product))a;
