14e9d2c8c8e9b47460191ad60152472679bc7133
Ajay Kumar Raju Kalidindi <kumar.ajay44499@gmail.com>
2023-07-15 16:19:16 -0400

    Update managers_with_atleast_5_direct_reports.sql

--- managers_with_atleast_5_direct_reports.sql
+++ managers_with_atleast_5_direct_reports.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select name from(
-select e1.name name,count(e2.name)  from Employee e1
+select e1.id,e1.name "name",count(e2.name)  from Employee e1
 join Employee e2
 on e1.id=e2.managerId
-group by e1.name
+group by e1.id,e1.name
 having count(e2.name)>=5
 )a;
